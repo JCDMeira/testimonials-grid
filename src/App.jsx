78d6bac83a1a1532cb81a83156c1251ad530f6dd
Jean Meira <jean.meira10@hotmail.com>
2021-10-01 16:34:51 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="conteiner">
+        <div className="content">
+          <h1>hellor world</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
