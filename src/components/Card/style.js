f855ae77a527ed526295b91779b65f8aada82244
Jean Meira <jean.meira10@hotmail.com>
2021-10-04 10:27:54 -0300

    Fix: text size for fit last card

--- src/components/Card/style.js
+++ src/components/Card/style.js
@@ -4,7 +4,7 @@ export const Fieldcard = styled.div`
   background: ${(props) => props.background};
   height: ${(props) => `${props.height}rem`};
   border-radius: 1rem;
-  padding: 2.5rem 2.5rem 2.5rem 3.1rem;
+  padding: 2.5rem 2.5rem 0rem 3.1rem;
   filter: drop-shadow(0px 8px 12px rgba(0, 0, 0, 0.25));
   margin-bottom: 2.4rem;
   overflow: auto;
